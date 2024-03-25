2e66ccf4a74c7602985e5c7a031e0a67c7df243d
hsunnie <gustjs2513@naver.com>
2024-03-25 12:05:00 +0900

    update

--- hsunnie/crawl_report/crawl_program_final.py
+++
@@ -1,8 +0,0 @@
-# 크롤링 프로그램
-from crawl1 import crawl_info
-from crawl2 import pdf_downloader
-from crawl3 import pdf_to_txt
-
-csv_file_name = crawl_info() # csv 파일 이름 반환
-pdf_downloader(csv_file_name) # pdf 파일 다운로드
-pdf_to_txt(csv_file_name) # pdf 파일을 각각 txt 파일로 변환하여 저장
